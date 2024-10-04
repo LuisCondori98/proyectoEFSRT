c781eb665a89ba4440f3e619d233d97a5c99a763
luiscondori98 <lcondori11@hotmail.com>
2024-10-04 17:08:43 -0500

    arreglando barra de navegacion

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,39 +7,72 @@ const NavBar = () => {
 
   const {totalQuantity} = useContext(CartContext)
 
+  const toggleMenu = () => {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+      sidebar.style.width = "0";
+    } else {
+      sidebar.style.width = "250px";
+    }
+  }
+  
+
   return (
     <div className="container-navbar">
-      <h1 to={"/"} className="title">GAMARUCCI</h1>
-      {/* <Link to={"/cart"} class="bi bi-cart4 cart" style={{fontSize: "35px", margin: "0 20px"}}></Link> */}
-      {
-        totalQuantity > 0 ?
-        <div className="cart">
-          <Link to={"/cart"} class="bi bi-cart4" style={{color: "black"}}></Link><h6 style={{color: "black"}}>{totalQuantity}</h6>
-        </div>
-        :
-        <Link to={"/cart"} class="bi bi-cart4 cart" ></Link>
-      }
+        <span style={{padding: "10px"}} class="menu-btn" onClick={() => toggleMenu()}>☰</span>
+        <h1 to={"/"} className="title">GAMARUCCI</h1>
+        {
+         totalQuantity > 0 ?
+         <div className="cart">
+           <Link to={"/cart"} class="bi bi-cart4" style={{color: "black"}}></Link><h6 style={{color: "black"}}>{totalQuantity}</h6>
+         </div>
+         :
+         <Link to={"/cart"} class="bi bi-cart4 cart" ></Link>
+        }
       <Link to={"/user"} class="bi bi-person user"></Link>
-      <nav className="navbar navbar-expand-lg" id="navbar">
-        <div className="container-fluid">
-          <label className="navbar-brand" ></label>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } aria-current="page" to={"/"}>Home</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/productos"}>Productos</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/coleccion"}>Coleccion</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/bands"}>Bands</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/new-arrivals"}>New Arrivals</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/contact"}>Contact</NavLink>
-              <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/blog"}>Blog</NavLink>
-            </div>
-          </div>
-        </div>
+      <nav id="sidebar" class="sidebar" style={{zIndex: "99"}}>
+        <a href="javascript:void(0)" class="close-btn" onClick={() => toggleMenu()}>×</a>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } aria-current="page" to={"/"}>Home</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/productos"}>Productos</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/coleccion"}>Coleccion</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/bands"}>Bands</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/new-arrivals"}>New Arrivals</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/contact"}>Contact</NavLink>
+        <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/blog"}>Blog</NavLink>
       </nav>
-    </div>
+      </div>
+    // <div className="container-navbar">
+    //   <h1 to={"/"} className="title">GAMARUCCI</h1>
+    //   {/* <Link to={"/cart"} class="bi bi-cart4 cart" style={{fontSize: "35px", margin: "0 20px"}}></Link> */}
+    //   {
+    //     totalQuantity > 0 ?
+    //     <div className="cart">
+    //       <Link to={"/cart"} class="bi bi-cart4" style={{color: "black"}}></Link><h6 style={{color: "black"}}>{totalQuantity}</h6>
+    //     </div>
+    //     :
+    //     <Link to={"/cart"} class="bi bi-cart4 cart" ></Link>
+    //   }
+    //   <Link to={"/user"} class="bi bi-person user"></Link>
+    //   <nav className="navbar navbar-expand-lg" id="navbar">
+    //     <div className="container-fluid">
+    //       <label className="navbar-brand" ></label>
+    //       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
+    //       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    //         <div className="navbar-nav">
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } aria-current="page" to={"/"}>Home</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/productos"}>Productos</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/coleccion"}>Coleccion</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/bands"}>Bands</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/new-arrivals"}>New Arrivals</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/contact"}>Contact</NavLink>
+    //           <NavLink className={({isActive}) => isActive? "nav-link link-active" : "nav-link" } to={"/blog"}>Blog</NavLink>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </div>
   )
 }
 
